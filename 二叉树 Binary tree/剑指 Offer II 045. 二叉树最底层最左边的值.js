2814a54d5e5f21e4a80cb0f4ca30efe7f64828dc
8ecomeDreamer <1412415464@qq.com>
2022-09-01 14:14:04 +0800

    二叉树最底层最左边的值~

--- 二叉树 Binary tree/剑指 Offer II 045. 二叉树最底层最左边的值.js
+++ 二叉树 Binary tree/剑指 Offer II 045. 二叉树最底层最左边的值.js
@@ -11,3 +11,38 @@
 
 // 输入: [1,2,3,4,null,5,6,null,null,7]
 // 输出: 7
+
+// **
+//  * Definition for a binary tree node.
+//  * function TreeNode(val, left, right) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.left = (left===undefined ? null : left)
+//  *     this.right = (right===undefined ? null : right)
+//  * }
+//  */
+//  
+//  * @param {TreeNode} root
+//  * @return {number}
+
+//思路：  bfs
+var findBottomLeftValue = function(root) {
+    let tempQueue = [root]
+    let lastleftnode=0
+    while(tempQueue.length){
+        let node = tempQueue.shift()
+        if(node.right){
+            tempQueue.push(node.right)
+        } 
+        if(node.left) {
+            tempQueue.push(node.left)
+        }
+        // 判断到达最后一层时使用全局变量接受 换个思路最后的左节点肯定最后一个进
+        lastleftnode=node.val
+    }
+    return lastleftnode
+
+};
+
+// 归纳总结：
+// 算法的本质就是穷举，在二叉树中体会的特别深刻。面对二叉树的题可以想想使用dfs还是bfs比较好
+// 本题思路在于最左节点是最后一个进 想到了就很简单。
